0ecda50f96e1a70e8a92b23be61d3707fd32500a
IvanCMC37 <iot3534199@gmail.com>
2022-02-01 14:36:06 +1100

    Fixed minor bug on sql query

--- controllers/orders.go
+++ controllers/orders.go
@@ -22,13 +22,19 @@ func GetOrders(c *gin.Context) {
 
 	var results []Result
 
-	models.DB.Table("companies").Select("ORDER_NAME, COMPANY_NAME, NAME, ORDER_DATE, LOWER(string_agg(product,';')) as PRODUCTS, sum(DISTINCT(quantity*price_per_unit)) as TOTAL" +
-		",sum(DISTINCT(quantity)) as ORDER_QUANTITY, sum(delivered_quantity) as DELIVERED_QUANTITY, sum(DELIVERED_QUANTITY*price_per_unit) as DELIVERED_AMOUNT").
+	models.DB.Table("companies").Select("ORDER_ID,ORDER_NAME, COMPANY_NAME, NAME, string_agg(DISTINCT(ORDER_DATE),';') as ORDER_DATE, string_agg(DISTINCT(product),';')" +
+		" as PRODUCTS, sum(DISTINCT(TOTAL)) as TOTAL,sum(DISTINCT(quantity)) as ORDER_QUANTITY, sum(delivered_quantity) as DELIVERED_QUANTITY, sum(DELIVERED_AMOUNT) as DELIVERED_AMOUNT").
 		Joins("INNER join customers on companies.COMPANY_ID = customers.COMPANY_ID").
-		Joins("INNER join orders on orders.CUSTOMER_ID = customers.USER_ID").
-		Joins("INNER join order_items on order_items.order_id = orders.id").
-		Joins("INNER join deliveries as d on d.order_item_id = order_items.id").
-		Group("order_items.order_id, ORDER_NAME,COMPANY_NAME,NAME, ORDER_DATE").Scan(&results)
+		Joins("INNER JOIN(select o.id as ORDER_ID, string_agg(DISTINCT(order_date),';') as ORDER_DATE, order_name, customer_id, sum(DISTINCT(quantity*price_per_unit)) as total" +
+			",LOWER(string_agg(distinct(product),';')) as PRODUCT, sum(DISTINCT(quantity)) as quantity" +
+			",sum(coalesce(delivered_quantity,0)) as DELIVERED_QUANTITY" +
+			",sum(coalesce(delivered_quantity,0)*price_per_unit) as DELIVERED_AMOUNT" +
+			" from orders as o" +
+			" join order_items as oi on oi.order_id=o.id" +
+			" left join deliveries as d on d.order_item_id = oi.id" +
+			" group by o.id,order_name,customer_id,oi.id,d.order_item_id) as foo on foo.CUSTOMER_ID = customers.USER_ID").
+		Group("ORDER_ID, ORDER_NAME,COMPANY_NAME,NAME").
+		Order("ORDER_ID").Scan(&results)
 
 	c.JSON(http.StatusOK, gin.H{
 		"data": results,
